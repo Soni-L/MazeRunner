c81844dd5cf755a422371b50376d283c9f21ac7f
soni lamkaj <acdsoni@gmail.com>
2020-04-18 14:13:53 +0200

    added visit function and imporved import syntax

--- Helpers.js
+++ Helpers.js
@@ -1,7 +1,7 @@
-//takes [row, col] tuple of coordinates of a singular location in the maze
+//receives params: [row, col] of a maze coordinate
 //explores the possible paths to move forward from that point -> down, left, up, right (NOT diagonal!)
-//returns an array of all paths forward [[row, colon], ...]
-module.exports.explore = (coordinates, workingMaze) => {
+//returns an array of all paths forward [[row, colon], [row, colon], ...]
+const explore = (coordinates, workingMaze) => {
   let [row, col] = coordinates;
   let pathsFowardArray = [];
 
@@ -27,3 +27,24 @@ module.exports.explore = (coordinates, workingMaze) => {
 
   return pathsFowardArray;
 };
+
+
+//1) receives the current coordinate and an array of all possible moves
+//2) visits the next coordinate in the array, if there is one, marks the current coordinate with a (1)
+//   as visited and pushes the next coordinate on the stack to be explored
+//-if there are several possible paths, the visitation is done in clockwise order -> down, left, up, right
+const visit = (currentCoordinate, coordinatePathsArray, workingMaze, mazePathStack) => {
+  //mark current coordinate
+  [currentRow, currentCol] = currentCoordinate;
+  workingMaze[currentRow][currentCol] = 1;
+
+  //move to the next
+  if (coordinatePathsArray.length >= 1) {
+    [nextRow, nextCol] = coordinatePathsArray[0];
+    mazePathStack.push([nextRow, nextCol]);
+  }
+
+  return[workingMaze, mazePathStack];
+};
+
+module.exports = [visit, explore];
--- MazeRunner.js
+++ MazeRunner.js
@@ -1,31 +1,16 @@
-const explore = require("./Helpers").explore;
+const [visit, explore] = require("./Helpers");
 
 //#region shared memory - we use these variables as global memory for the algorithm
 //The maze currently being worked on, (this will mutate state)
 let workingMaze = [];
 
-//This is an array that we will use like a stack (only push and pop ops allowed)
+//This is an array that we will use like a stack for the DFS (only push and pop ops allowed)
 let mazePathStack = [];
 
 //The que for the BFS algorithm
 let mazePathQue = [];
 
 //#endregion
-
-//1) receives the current coordinate and an array of all possible moves
-//2) visits the next coordinate in the array
-//--if there are several possible paths, the visitation is done in this order going clockwise -> down, left, up, right
-//--if there is only one potential path from the current coordinate or no paths at all, marks the current coordinate as visited (1) 
-//  and moves to the next (0) coordinate, if there is one.
-const visit = (currentCoordinate, coordinatePathsArray) => {
-  let visitForwardCoord = [];
-  [currentRow, currentCol] = currentCoordinate;
-
-  if ((coordinatePathsArray.length = 1)) {
-    inputMaze[currentRow][currentRow] = 1;
-  }
-};
-
 
 //Start Here!
 (() => {
@@ -45,10 +30,17 @@ const visit = (currentCoordinate, coordinatePathsArray) => {
   //workingMaze init
   workingMaze = inputMaze;
 
-  //// Step 2
-  //We initialize the maze_path stack with the entry coordinate:[row, column] of the inputMaze
+  // Step 2
+  //We initialize the mazePathStack with the entry coordinate:[row, column] of the inputMaze
   mazePathStack.push([0, 1]);
 
-  //
-  console.log(explore([0, 1], workingMaze));
+  [workingMaze, mazePathStack] = visit(
+    [0, 1],
+    [[1, 1]],
+    workingMaze,
+    mazePathStack
+  );
+  console.log(workingMaze);
+  console.log(mazePathStack);
+  //console.log(explore([0, 1], workingMaze));
 })();
